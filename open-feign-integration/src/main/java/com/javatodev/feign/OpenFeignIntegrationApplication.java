06bfeec9edbc1802831dc86d2e8c0dfd10f78540
javatodev <sendtospringboot@gmail.com>
2020-10-03 01:20:09 +0530

    open feign

---
+++ open-feign-integration/src/main/java/com/javatodev/feign/OpenFeignIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javatodev.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignIntegrationApplication.class, args);
+    }
+
+}
---
+++ open-feign-integration/src/test/java/com/javatodev/feign/OpenFeignIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatodev.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
