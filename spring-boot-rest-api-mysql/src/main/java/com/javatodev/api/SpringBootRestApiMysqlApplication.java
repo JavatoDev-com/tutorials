45841417b5888cd1502009d5a7bc6cad27e648fc
javatodev <sendtospringboot@gmail.com>
2020-10-14 00:14:16 +0530

    #spring boot rest api mysql

---
+++ spring-boot-rest-api-mysql/src/main/java/com/javatodev/api/SpringBootRestApiMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.javatodev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiMysqlApplication.class, args);
+    }
+
+}
