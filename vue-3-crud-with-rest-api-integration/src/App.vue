ecb870176f715c97207ed856166dcf0c37de631e
javatodev <sendtospringboot@gmail.com>
2020-11-29 01:10:14 +0530

    dashboard

--- vue-3-crud-with-rest-api-integration/src/App.vue
+++ vue-3-crud-with-rest-api-integration/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Dashboard/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Dashboard from './components/Dashboard.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Dashboard,
   },
 })
 export default class App extends Vue {}
---
+++ vue-3-crud-with-rest-api-integration/src/components/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Dashboard extends Vue{
+
+}
+
+</script>
